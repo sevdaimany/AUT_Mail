cd785c502da953e159054da2e352f548d98364e1
sevda <imanysevda@gmail.com>
2022-08-09 16:28:06 +0430

    cleaning code

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #%%
+import re
 import telebot
 from telebot.types import ForceReply
 import requests
@@ -17,7 +18,7 @@ URL = "http://127.0.0.1:5000"
 
 def force_reply():
     reply = ForceReply()
-    reply.input_field_placeholder = "Username and password with ,"
+    reply.input_field_placeholder = "MewWwW"
     return reply
 
 
@@ -25,7 +26,7 @@ def username_handler(message):
     global username
     username = message.text
     
-    sent_msg = bot.send_message(message.chat.id, f"Your username is {username}. what is your password?")
+    sent_msg = bot.send_message(message.chat.id, f"Enter your Password:", reply_markup=force_reply())
     bot.register_next_step_handler(sent_msg, password_handler)
 
    
@@ -33,38 +34,20 @@ def username_handler(message):
 def password_handler(message):
     global password
     password = message.text
-    bot.send_message(message.chat.id, f"Your password is {password}")
     requests.post(f"{URL}/driver", json={"username": username, "password": password, "chatid": message.chat.id})
-
-
 
 
 @bot.message_handler(commands=["start", "newInfo"])
 def start(message):
-    sent_msg = bot.send_message(message.chat.id, "Please Enter your username:")
+    sent_msg = bot.send_message(message.chat.id, "Please Enter your username:", reply_markup=force_reply())
     bot.register_next_step_handler(sent_msg, username_handler)
     
-    
-    
-            
-            
-            
-  
-
-def send_unseen_emails(message, allEmails, newEmailIDs):
-    for email in allEmails:
-        if email["isSeen"] == "no" and (email["id"] in newEmailIDs):
-            text = f"From : {email['from']}\nSubject : {email['subject']}\nSnippet : {email['snippet']}\nDate: {email['date']}"
-            bot.send_message(message.chat.id, text)
-
-
 
 
 @bot.message_handler(func=lambda message: True)
 def recieve_cred(message):
     user_input = message.text
     if len(user_input) == 4:
-
         r = requests.post(f"{URL}/captcha", json={"captcha":user_input, 'chatid':message.chat.id})
         
 
